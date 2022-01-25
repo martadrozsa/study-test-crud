42bed07b727e9e5623af121f54267ad7046accaa
martadrozsa <marta.denisczwicz@gmail.com>
2022-01-25 08:18:33 -0300

    Project created

---
+++ src/main/java/com/denisczwicz/studytestcrud/StudyTestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.denisczwicz.studytestcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTestCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denisczwicz/studytestcrud/StudyTestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisczwicz.studytestcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
